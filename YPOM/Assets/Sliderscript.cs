72daf6fb4b99ba76078182b21c2de3c891ab66c5
Pavel Yudin <pavel.u24@gmail.com>
2019-04-05 22:55:32 +0400

    Healthbar

---
+++ YPOM/Assets/Sliderscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliderscript : MonoBehaviour
+{
+    GameObject player;
+    HealthPoints health;
+    Slider healthbar;
+    void Awake()
+    {
+        health = GameObject.Find("Cube").GetComponent<HealthPoints>();
+        healthbar = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        healthbar.value = health.healthp;
+    }
+}
