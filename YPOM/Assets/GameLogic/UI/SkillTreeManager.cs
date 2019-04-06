c3bed2fcd5c2488a921df3e10560e3fe492c7d62
Sadpotato <alexprgrigoriev@gmail.com>
2019-04-06 12:45:21 +0400

    dino animated & tree created

---
+++ YPOM/Assets/GameLogic/UI/SkillTreeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeManager : MonoBehaviour
+{
+    public GameObject skillTree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skillTree.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Sunset.day)
+        {
+            if (!skillTree.activeInHierarchy)
+            {
+                skillTree.SetActive(true);
+            }
+        }
+    }
+}
