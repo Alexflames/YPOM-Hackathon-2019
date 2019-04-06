6364d147bc9b30dd364dc07d8fe47486a3f65749
Ilya <brineone@gmail.com>
2019-04-06 19:31:06 +0400

    На разбор гениальному мужику!

---
+++ YPOM/Assets/GameLogic/Bots/Bosses/Boss_Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Bullet : MonoBehaviour
+{
+    Transform target; //цель
+    float speed = 10; //скорость полета пули
+    //int damage = ; //урон от одной пули
+    Vector3 pos;
+    public GameObject Puddle;
+    bool flag_puddle = true;
+    HealthPoints stats;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < 1f) //если пуля достигла цели
+        {
+            stats = target.GetComponent<HealthPoints>();
+            stats.healthp -= 5;
+            Destroy(gameObject); //удаление пули
+        }
+        if (Vector3.Distance(transform.position, pos) > 0.5f) //пока пуля не достигла места назначения
+            Move(); //движение пули
+        else
+            Destroy(gameObject); //удаление пули
+    }
+
+    public void SetTarget(Transform enemy) //установка цели
+    {
+        target = enemy;
+        pos = target.position; //позиция объекта (на момент выстрела)
+        pos.y -= 0.5f; //высота персонажа
+    }
+
+    void Move()
+    {
+        Vector3 dir = pos - transform.position;
+        transform.Translate(dir.normalized * Time.deltaTime * speed); //перемещение
+    }
+}
---
+++ YPOM/Assets/GameLogic/Bots/Bosses/Boss_Shooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Shooting : MonoBehaviour
+{
+    public float CurrCooldown; //временное КД
+    public float Cooldown; //общее КД
+    public GameObject Bullet;
+    Boss_Move vrag;
+
+    void Update()
+    {
+        if (CanShoot()) //если можно стрелять
+            SearchTarget(); //поиск врага
+
+        if (CurrCooldown > 0) //есть ли КД
+            CurrCooldown -= Time.deltaTime; //уменьшение КД
+    }
+
+    bool CanShoot() //может ли враг стрелять в данный момент
+    {
+        if (CurrCooldown <= 0) //есть ли КД
+            return true;
+        return false;
+    }
+
+    void SearchTarget() //поиск цели
+    {
+        vrag = gameObject.GetComponent<Boss_Move>();
+        GameObject enemy = vrag.min_dist; //поиск врага (гг)
+        var range = GetComponentInChildren<CapsuleCollider>();
+        float currDistance = Vector3.Distance(transform.position, enemy.transform.position);
+        if (currDistance <= range.radius) //если враг в радиусе
+            Shoot(enemy.transform); //выстрел по нему
+    }
+
+    void Shoot(Transform enemy) //функция выстрела
+    {
+        CurrCooldown = Cooldown; //обновление КД
+        GameObject bul = Instantiate(Bullet); //создание пули
+        bul.transform.position = transform.position; //место появления пули
+        bul.GetComponent<Bullet>().SetTarget(enemy);
+    }
+}
